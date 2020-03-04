7269ccfb31aa081e6b0aeae39a61c95b577b5136
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-04 20:54:23 +0800

    Update ssr-caddy-tls.sh

--- ssr-caddy-tls.sh
+++ ssr-caddy-tls.sh
@@ -17,28 +17,25 @@ echo "http://$domainname:80 {
 } 
 https://$domainname:443 {  
         gzip  
-		tls $emailname
-        root /var/www/$domainname 
-        
+	tls $emailname
+        root /var/www/$domainname     
 }" > /etc/caddy/Caddyfile
-sleep 10
+
 echo "[program:caddy]
-command = /usr/local/bin/caddy -agree -conf /etc/caddy/Caddyfile
-process_name = caddy
-stopwaitsecs = 11
-directory = /usr/local/bin/caddy
-redirect_stderr=true
-stopwaitsecs = 11" > /etc/supervisor/conf.d/caddy.conf
- sleep 10
+command = /usr/local/bin/caddy -log stdout -agree=true -conf=/etc/caddy/Caddyfile
+directory = /etc/caddy
+autorstart=true
+environment=CADDYPATH=/etc/ssl/caddy" > /etc/supervisor/conf.d/caddy.conf
+supervisord -c /etc/supervisor/supervisord.conf
 
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
 
 sed '4c "server_port":443,' /etc/shadowsocks-r/config.json
-sleep 10
+
 sed '14c "redirect": ["*:443#127.0.0.1:$port"],' /etc/shadowsocks-r/config.json
-sleep 10
+
 sudo /etc/init.d/shadowsocks-r restart
 
 echo net.core.default_qdisc=fq >> /etc/sysctl.conf
@@ -54,7 +51,7 @@ caddy 安装和配置成功
 启动：supervisorctl start caddy  
 停止：supervisorctl stop caddy    
 重启：supervisorctl restart caddy  
-查看状态：caddy -service status  
+查看状态：supervisorctl status 
 安装目录为：/usr/local/bin/caddy 
 配置文件位置：/etc/caddy/Caddyfile
 *****************************************
