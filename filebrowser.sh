122ee47d95d9da954df834b88938e3ef4870c718
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-14 20:00:44 +0800

    Update filebrowser.sh

--- filebrowser.sh
+++ filebrowser.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 curl -fsSL https://filebrowser.xyz/get.sh | bash
-cd /var/www
 mkdir files
-filebrowser -d /var/www/filebrowser.db config init
-filebrowser -d /var/www/filebrowser.db config set --address 0.0.0.0
+filebrowser -d /etc/filebrowser.db config init
+filebrowser -d /etc/filebrowser.db config set --address 0.0.0.0
 echo "请输入一个1-65535之间的端口号:"
 read port
 while [ $port -le 0 -o $port -gt 65535 ]
@@ -11,27 +10,28 @@ do
 	echo "端口必须在1-65535之间。请重新输入:"
 	read port
 done
-filebrowser -d /var/www/filebrowser.db config set --port $port
-filebrowser -d /var/www/filebrowser.db config set --locale zh-cn
-filebrowser -d /var/www/filebrowser.db config set --log /var/log/filebrowser.log
+filebrowser -d /etc/filebrowser.db config set --port $port
+filebrowser -d /etc/filebrowser.db config set --locale zh-cn
+filebrowser -d /etc/filebrowser.db config set --log /var/log/filebrowser.log
 echo "输入用户名:"
 read $user
 echo "输入密码:"
 read $pswd
-filebrowser -d /var/www/filebrowser.db users add $user $pswd --perm.admin
+filebrowser -d /etc/filebrowser.db users add $user $pswd --perm.admin
+
 echo "[Unit]
 Description=File Browser
 After=network.target
 
 [Service]
-ExecStart=/usr/local/bin/filebrowser -d /var/www/filebrowser.db
+ExecStart=/usr/local/bin/filebrowser -d /etc/filebrowser.db
 
 [Install]
 WantedBy=multi-user.target" > /lib/systemd/system/filebrowser.service
 systemctl daemon-reload
 systemctl stop filebrowser.service
 systemctl start filebrowser.service
-sed -i '14i proxy / 127.0.0.1:$port' /etc/caddy/Caddyfile 
+sed -i '14i proxy /admin 127.0.0.1:$port' /etc/caddy/Caddyfile 
 supervisorctl restart caddy
 echo "运行：systemctl start filebrowser.service
 停止运行：systemctl stop filebrowser.service
