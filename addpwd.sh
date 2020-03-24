81379d0a018d49f1e44a30cb9b87c1d1727c0885
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-24 14:31:03 +0800

    Update addpwd.sh

--- addpwd.sh
+++ addpwd.sh
@@ -3,5 +3,6 @@
 sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 passwd root
-/etc/init.d/ssh restart
+systemctl restart sshd.service
+systemctl enable sshd.service
 echo '修改成功，请用用户名root和刚设置好的密码登录vps吧，enjoy'
