6c7c8ec231d457ad203420f992a8f8efefe27b35
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-25 17:47:39 +0800

    Update ssmgr.sh

--- ssmgr.sh
+++ ssmgr.sh
@@ -79,8 +79,8 @@ get_ip(){
 
 config(){
   # download template file
-  wget https://raw.githubusercontent.com/shellhub/shellhub/master/ssmgr/ss.template.yml
-  wget https://raw.githubusercontent.com/shellhub/shellhub/master/ssmgr/webgui.template.yml
+  wget https://raw.githubusercontent.com/JeannieStudio/jeannie/master/ss.template.yml
+  wget https://raw.githubusercontent.com/JeannieStudio/jeannie/master/webgui.template.yml
 
   # write webgui password
   read -p "Input webgui manage password:" password
