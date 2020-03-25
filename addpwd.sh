5e25c23b533fc303728351e4542079be073dedee
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-25 16:06:33 +0800

    Update addpwd.sh

--- addpwd.sh
+++ addpwd.sh
@@ -38,9 +38,13 @@ install_wget(){
     yum -y install wget
   fi
 }
-sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-passwd root
-systemctl restart sshd.service
-systemctl enable sshd.service
-echo '修改成功，请用用户名root和刚设置好的密码登录vps吧，enjoy'
+main(){
+    install_wget
+    sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+    passwd root
+    systemctl restart sshd.service
+    systemctl enable sshd.service
+    echo '修改成功，请用用户名root和刚设置好的密码登录vps吧，enjoy'
+}
+main()
