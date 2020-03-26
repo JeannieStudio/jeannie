4428f54b191d066e5d9e10631f85da87cce2cd97
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-26 09:52:47 +0800

    Update addpwd.sh

--- addpwd.sh
+++ addpwd.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-
+isRoot() {
+  if [[ "$EUID" -ne 0 ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+}
+main(){
+  #check root permission
+  isRoot=$( isRoot )
+  if [[ "${isRoot}" != "true" ]]; then
+    echo -e "${RED_COLOR}error:${NO_COLOR}Please run this script as as root"
+    exit 1
+  else
     sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     passwd root
     systemctl restart sshd.service
     systemctl enable sshd.service
     echo '修改成功，请用用户名root和刚设置好的密码登录vps吧，enjoy'
+  fi
+}
+main
   
 
