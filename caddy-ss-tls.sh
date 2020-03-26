7d2a4476b46364e034e6bf21fc58ab17c6ea2e3b
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-03-26 17:53:14 +0800

    Update and rename caddy-ss-tls to caddy-ss-tls.sh

--- caddy-ss-tls.sh
+++ caddy-ss-tls.sh
@@ -1,6 +1,49 @@
 #!/bin/bash
-curl https://getcaddy.com | bash -s personal 
-yum install supervisor
+init_release(){
+  if [ -f /etc/os-release ]; then
+      # freedesktop.org and systemd
+      . /etc/os-release
+      OS=$NAME
+  elif type lsb_release >/dev/null 2>&1; then
+      # linuxbase.org
+      OS=$(lsb_release -si)
+  elif [ -f /etc/lsb-release ]; then
+      # For some versions of Debian/Ubuntu without lsb_release command
+      . /etc/lsb-release
+      OS=$DISTRIB_ID
+  elif [ -f /etc/debian_version ]; then
+      # Older Debian/Ubuntu/etc.
+      OS=Debian
+  elif [ -f /etc/SuSe-release ]; then
+      # Older SuSE/etc.
+      ...
+  elif [ -f /etc/redhat-release ]; then
+      # Older Red Hat, CentOS, etc.
+      ...
+  else
+      # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+      OS=$(uname -s)
+  fi
+
+  # convert string to lower case
+  OS=`echo "$OS" | tr '[:upper:]' '[:lower:]'`
+
+  if [[ $OS = *'ubuntu'* || $OS = *'debian'* ]]; then
+    PM='apt'
+  elif [[ $OS = *'centos'* ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+}
+if [[ ${PM} = "apt" ]]; then
+    apt-get install supervisor   
+  elif [[ ${PM} = "yum" ]]; then
+    yum install supervisor
+  fi
+
+
 mkdir /etc/caddy
 mkdir /etc/ssl/caddy
 mkdir /var/www
