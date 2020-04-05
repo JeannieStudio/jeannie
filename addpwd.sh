d61389058fef20a6eb4677ca6b36f0e6535c83a5
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-06 00:49:01 +0800

    Update addpwd.sh

--- addpwd.sh
+++ addpwd.sh
@@ -6,6 +6,39 @@ isRoot() {
     echo "true"
   fi
 }
+init_release(){
+  if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+    fi
+  if [[ $release = "ubuntu" || $release = "debian" ]]; then
+    PM='apt'
+  elif [[ $release = "centos" ]]; then
+    PM='yum'
+  else
+    exit 1
+  fi
+  # PM='apt'
+}
+tools_install(){
+  init_release
+  if [ $PM = 'apt' ] ; then
+    apt-get install -y dnsutils wget unzip zip curl tar git
+  elif [ $PM = 'yum' ]; then
+    yum -y install bind-utils wget unzip zip curl tar git
+  fi
+}
 main(){
   #check root permission
   isRoot=$( isRoot )
@@ -13,14 +46,19 @@ main(){
     echo -e "非root用户不能执行该脚本"
     exit 1
   else
+    tools_install
     sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     passwd root
-    systemctl restart sshd.service
+    init_release
+    if [ $PM = 'apt' ] ; then
+      service sshd restart
+    elif [ $PM = 'yum' ]; then
+     systemctl restart sshd.service
     systemctl enable sshd.service
-    service sshd restart
+    fi
     echo '修改成功，请用用户名root和刚设置好的密码登录vps吧，enjoy'
   fi
 }
