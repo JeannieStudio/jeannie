4b561234ee416895b1e104279d41e6eaf10896d1
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-08 18:20:28 +0800

    Update v2ray-caddy-tls-ws.sh

--- v2ray-caddy-tls-ws.sh
+++ v2ray-caddy-tls-ws.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-RED_COLOR="\033[0;31m"
+RED="\033[0;31m"
 NO_COLOR="\033[0m"
 GREEN="\033[0;32m"
+BLUE="\033[0;36m"
+green(){
+    echo -e "\033[32m\033[01m$1\033[0m"
+}
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 echo "等3秒……"
@@ -39,22 +43,44 @@ init_release(){
   fi
   # PM='apt'
 }
+tools_install(){
+  PID=$(ps -ef |grep "nginx" |grep "caddy"|grep -v "grep" |grep -v "init.d" |grep -v "service" |grep -v "caddy_install" |awk '{print $2}')
+	[[ ! -z ${PID} ]] && kill -9 ${PID}
+  init_release
+  nginx -s stop
+  if [ $PM = 'apt' ] ; then
+    apt-get install -y dnsutils wget unzip zip curl tar git
+  elif [ $PM = 'yum' ]; then
+    yum -y install bind-utils wget unzip zip curl tar git
+  fi
+  systemctl enable nginx.service
+}
+left_second(){
+    seconds_left=30
+    while [ $seconds_left -gt 0 ];do
+      echo -n $seconds_left
+      sleep 1
+      seconds_left=$(($seconds_left - 1))
+      echo -ne "\r     \r"
+    done
+}
  v2ray_install(){
    cp  /usr/share/zoneinfo/Asia/Shanghai  /etc/localtime
    service v2ray stop
    bash <(curl -L -s https://install.direct/go.sh)
  }
  caddy_install(){
-  PID=$(ps -ef |grep "caddy" |grep -v "grep" |grep -v "init.d" |grep -v "service" |grep -v "caddy_install" |awk '{print $2}')
-  [[ ! -z ${PID} ]] && kill -9 ${PID}
   curl https://getcaddy.com | bash -s personal hook.service
 }
 caddy_conf(){
   read -p "输入您的域名:" domainname
-  read -p "您输入的域名正确吗? [y/n]?" answer
-  if [ $answer != "y" ]; then
-     read -p "请重新输入您的域名:" domainname
-  fi
+  real_addr=`ping ${domainname} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+  local_addr=`curl ipv4.icanhazip.com`
+  while [ "$real_addr" != "$local_addr" ]; do
+     read -p "本机ip和绑定域名的IP不一致，请检查域名是否解析成功,并重新输入域名:" domainname
+     real_addr=`ping ${domainname} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
+     local_addr=`curl ipv4.icanhazip.com`
+  done
   read -p "请输入您的邮箱：" emailname
   read -p "您输入的邮箱正确吗? [y/n]?" answer
   if [ $answer != "y" ]; then
@@ -76,6 +102,13 @@ caddy_conf(){
            header_upstream -Origin
         }
        }" > /etc/caddy/Caddyfile
+}
+CA_exist(){
+  if [ -d "/root/.caddy/acme/acme-v02.api.letsencrypt.org/sites/$domainname" -o -d "/.caddy/acme/acme-v02.api.letsencrypt.org/sites/$domainname" ]; then
+    FLAG="YES"
+  else
+    FLAG="NO"
+  fi
 }
 genId(){
     id1=$(cat /proc/sys/kernel/random/uuid | md5sum |cut -c 1-8)
@@ -120,46 +153,54 @@ filebrowser_install(){
     [Install]
     WantedBy=multi-user.target" > /lib/systemd/system/filebrowser.service
 }
-wget_install(){
-  init_release
-  if [ $PM = 'apt' ] ; then
-    apt-get install wget
-  elif [ $PM = 'yum' ]; then
-    yum -y install wget
-  fi
-}
 main(){
    isRoot=$( isRoot )
   if [[ "${isRoot}" != "true" ]]; then
     echo -e "${RED_COLOR}error:${NO_COLOR}Please run this script as as root"
     exit 1
   else
-  wget_install
+  tools_install
   caddy_install
   caddy_conf
+  caddy -service stop
+  caddy -service uninstall
+  caddy -service install -agree -email ${emailname} -conf /etc/caddy/Caddyfile
+  echo "睡一会儿……"
+  left_second
+  caddy -service start
   v2ray_install
   v2ray_conf
   filebrowser_install
   service v2ray start
-  caddy -service stop
-  caddy -service uninstall
-  caddy -service install -agree -email ${emailname} -conf /etc/caddy/Caddyfile
-  caddy -service start
-  caddy -service restart
   systemctl start filebrowser.service
   systemctl enable filebrowser.service
-  echo -e "${GREEN}恭喜你，v2ray安装和配置成功
-域名:        ${GREEN}${domainname}
-端口:        ${GREEN}443
-UUID:        ${GREEN}${id}
-混淆:        ${GREEN}websocket
-路径：       ${GREEN}/ray
-访问：${GREEN}https://${domainname}  体验个人在线私有云盘.
-登录名：$user ，密码：$pswd。
-Windows、Macos客户端下载v2ray-core： ${GREEN}https://github.com/v2ray/v2ray-core/releases
-安卓客户端下载v2rayNG: ${GREEN}https://github.com/2dust/v2rayNG/releases
-ios客户端请到应用商店下载：shadowrocket
-喜欢的话记得视频点赞、分享或订阅jeannie studio：${GREEN}https://bit.ly/2X042ea ${NO_COLOR}" 2>&1 | tee info
+  systemctl daemon-reload
+  CA_exist
+  if [ $FLAG = "YES" ]; then
+    green "=========================================="
+	  green "       恭喜你，v2ray安装和配置成功"
+	  green "=========================================="
+      echo -e "
+$BLUE域名:        ${GREEN}${domainname}
+$BLUE端口:        ${GREEN}443
+${BLUE}UUID:      ${GREEN}${id}
+${BLUE}AlterID:   ${GREEN}64
+$BLUE混淆:        ${GREEN}websocket
+$BLUE路径：       ${GREEN}/ray
+$BLUE访问：${GREEN}https://${domainname}  体验个人在线私有云盘.
+$BLUE登录名：${GREEN} $user ，$BLUE密码：${GREEN} $pswd。
+${BLUE}Windows、Macos客户端下载v2ray-core： ${GREEN}https://github.com/v2ray/v2ray-core/releases
+${BLUE}安卓客户端下载v2rayNG: ${GREEN}https://github.com/2dust/v2rayNG/releases
+${BLUE}ios客户端请到应用商店下载：${GREEN}shadowrocket
+${BLUE}关注jeannie studio：${GREEN}https://bit.ly/2X042ea ${NO_COLOR}" 2>&1 | tee info
+  elif [ $FLAG = "NO" ]; then
+      echo -e "
+$RED==========================================
+$RED    很遗憾，v2ray安装和配置失败
+$RED==========================================
+${RED}由于证书申请失败，无法科学上网，请重装或更换一个域名重新安装， 详情：https://letsencrypt.org/docs/rate-limits/
+进一步验证证书申请情况，参考：https://www.ssllabs.com/ssltest/${NO_COLOR}" 2>&1 | tee info
+  fi
   touch /etc/motd
   cat info > /etc/motd
   fi
