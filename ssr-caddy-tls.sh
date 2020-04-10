f6331bcd2f136b9a4be012980e8249eb378c86ce
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-11 00:00:41 +0800

    Update ssr-caddy-tls.sh

--- ssr-caddy-tls.sh
+++ ssr-caddy-tls.sh
@@ -139,7 +139,7 @@ main(){
   caddy -service uninstall
   caddy -service install -agree -email ${emailname} -conf /etc/caddy/Caddyfile
   caddy -service start
-  echo -e " ${FUCHSIA}睡一会儿……千万别关，还没装完，还没装完，还没装完……"
+  echo -e " ${GREEN}睡一会儿……千万别关，还没装完，还没装完，还没装完……"
   left_second
   caddy -service start
   /etc/init.d/shadowsocks-r restart
