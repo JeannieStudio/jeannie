16ab2c167081bb77c984013246ceb271e59f2fa3
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-10 22:42:42 +0800

    Update ssr-caddy-tls.sh

--- ssr-caddy-tls.sh
+++ ssr-caddy-tls.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 RED_COLOR="\033[0;31m"
 NO_COLOR="\033[0m"
-GREEN="\033[0;32m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
 echo "等3秒……"
@@ -38,6 +39,33 @@ init_release(){
     exit 1
   fi
   # PM='apt'
+}
+tools_install(){
+  PID=$(ps -ef | grep "v2ray" | grep -v grep | awk '{print $2}')
+  [[ ! -z ${PID} ]] && kill -9 ${PID}
+  PID=$(ps -ef | grep "trojan" | grep -v grep | awk '{print $2}')
+  [[ ! -z ${PID} ]] && kill -9 ${PID}
+  PID=$(ps -ef | grep "nginx" | grep -v grep | awk '{print $2}')
+  [[ ! -z ${PID} ]] && kill -9 ${PID}
+  PID=$(ps -ef | grep "caddy" | grep -v grep | awk '{print $2}')
+	[[ ! -z ${PID} ]] && kill -9 ${PID}
+  init_release
+  if [ $PM = 'apt' ] ; then
+    apt-get update -y
+    apt-get install -y dnsutils wget unzip zip curl tar git
+  elif [ $PM = 'yum' ]; then
+    yum update -y
+    yum -y install bind-utils wget unzip zip curl tar git
+  fi
+}
+left_second(){
+    seconds_left=30
+    while [ $seconds_left -gt 0 ];do
+      echo -n $seconds_left
+      sleep 1
+      seconds_left=$(($seconds_left - 1))
+      echo -ne "\r     \r"
+    done
 }
 caddy_install(){
   curl https://getcaddy.com | bash -s personal hook.service
@@ -92,12 +120,19 @@ filebrowser_install(){
     [Install]
     WantedBy=multi-user.target" > /lib/systemd/system/filebrowser.service
 }
+check_CA(){
+    end_time=$(echo | openssl s_client -servername $domainname -connect $domainname:443 2>/dev/null | openssl x509 -noout -dates |grep 'After'| awk -F '=' '{print $2}'| awk -F ' +' '{print $1,$2,$4 }' )
+    end_times=$(date +%s -d "$end_time")
+    now_time=$(date +%s -d "$(date | awk -F ' +'  '{print $2,$3,$6}')")
+    RST=$(($(($end_times-$now_time))/(60*60*24)))
+}
 main(){
    isRoot=$( isRoot )
   if [[ "${isRoot}" != "true" ]]; then
     echo -e "${RED_COLOR}error:${NO_COLOR}Please run this script as as root"
     exit 1
   else
+  tools_install
   caddy_install
   caddy_conf
   ssr_install
@@ -105,6 +140,9 @@ main(){
   caddy -service stop
   caddy -service uninstall
   caddy -service install -agree -email ${emailname} -conf /etc/caddy/Caddyfile
+  caddy -service start
+  echo "睡一会儿……"
+  left_second
   caddy -service start
   /etc/init.d/shadowsocks-r restart
   caddy -service restart
@@ -114,15 +152,26 @@ main(){
   method=$(sed -n '9p' /etc/shadowsocks-r/config.json)
   Protocol=$(sed -n '10p' /etc/shadowsocks-r/config.json)
   obfs=$(sed -n '12p' /etc/shadowsocks-r/config.json)
+  check_CA
   echo -e "${GREEN}恭喜你，安装和配置成功
-域名:        ${GREEN}\"${domainname}\"
-端口:        ${GREEN}\"443\"
-密码:        ${GREEN}${pwd##*:}
-密码方式:    ${GREEN}${method##*:}
-协议:        ${GREEN}${Protocol##*:}
-混淆:        ${GREEN}${obfs##*:}
-访问：${GREEN}https://${domainname}  体验个人在线私有云盘.
-登录名：$user ，密码：$pswd。" 2>&1 | tee info
+${BLUE}域名:        ${GREEN}\"${domainname}\"
+${BLUE}端口:        ${GREEN}\"443\"
+${BLUE}密码:        ${GREEN}${pwd##*:}
+${BLUE}加密方式:    ${GREEN}${method##*:}
+${BLUE}协议:        ${GREEN}${Protocol##*:}
+${BLUE}混淆:        ${GREEN}${obfs##*:}
+${BLUE}访问：${GREEN}https://${domainname}  体验个人在线私有云盘.
+${BLUE}登录名：$user ，密码：$pswd。
+${GREEN}=========================================================
+${BLUE} Windows客户端请从这里下载： $GREEN  https://github.com/shadowsocksrr/shadowsocksr-csharp/releases
+${BLUE} macOS客户端请从这里下载: $GREEN https://github.com/qinyuhang/ShadowsocksX-NG-R/releases
+$BLUE ios客户端到应用商店下载：$GREEN shadowrocket;
+$BLUE 安卓请客户端下载：$GREEN https://github.com/shadowsocksrr/shadowsocksr-android/releases
+$BLUE 关注jeannie studio：$GREEN https://bit.ly/2X042ea
+${GREEN}=========================================================
+${GREEN}当前检测的域名： $domainname
+${GREEN}证书有效期剩余天数:  ${RST}
+${GREEN}不用担心，证书会自动更新" 2>&1 | tee info
   touch /etc/motd
   cat info > /etc/motd
   fi
