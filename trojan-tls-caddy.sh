e3183e7bd7dada925edaa419c1bd6223f0c293e9
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-14 14:47:08 +0800

    Update trojan-tls-caddy.sh

--- trojan-tls-caddy.sh
+++ trojan-tls-caddy.sh
@@ -51,7 +51,7 @@ init_release(){
 tools_install(){
   systemctl stop trojan
   nginx -s stop
-  service v2ray start
+  service v2ray stop
   caddy -service stop
   init_release
   if [ $PM = 'apt' ] ; then
