3dbbe8c7b6497d5402391ea104ae0f2370a37e53
Jeannie Studio <48536485+JeannieStudio@users.noreply.github.com>
2020-04-17 14:45:26 +0800

    Update ssr-caddy-tls-b.sh

--- ssr-caddy-tls-b.sh
+++ ssr-caddy-tls-b.sh
@@ -99,7 +99,7 @@ caddy_conf(){
 }
 ssr_install(){
   /etc/init.d/shadowsocks-r stop
-   wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+   wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/JeannieStudio/jeannie/master/shadowsocks-all.sh
    chmod +x shadowsocks-all.sh
    ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
     #分别将配置/etc/shadowsocks-r/config.json文件的第4行和第14行改为下面内容
