267221c2bbe9b219e6319cae07c6c6259f9de2b2
goodppgood <57130097+goodppgood@users.noreply.github.com>
2021-08-08 02:41:08 +0800

    Update ssr-caddy-tls-b.sh

--- ssr-caddy-tls-b.sh
+++ ssr-caddy-tls-b.sh
@@ -71,7 +71,11 @@ left_second(){
     done
 }
 caddy_install(){
-  curl https://getcaddy.com | bash -s personal hook.service
+  sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+  sudo apt update
+  sudo apt install caddy
 }
 caddy_conf(){
   read -p "输入您的域名:" domainname
